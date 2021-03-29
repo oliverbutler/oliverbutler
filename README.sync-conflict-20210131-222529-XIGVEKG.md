10c63f4320eab44598769130e733a93a5b158717
Oliver Butler <dev@oliverbutler.uk>
2021-03-29 23:34:23 +0100

    🚧 Unknown old changes

---
+++ README.sync-conflict-20210131-222529-XIGVEKG.md
@@ -0,0 +1,10 @@
+## Hi, I'm Olly.
+
+- ❤️ **Loves** Open Source 
+- 👨‍🎓 Integrated Masters Student at Newcastle University
+- 👨‍💻 Currently developing [CardCollab](https://github.com/cardcollab)
+- 🌱 Learning AWS Cloud Development
+
+*Too many projects, not enough time.*
+
+
---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Title from "./components/Title";
+import RotatingText from "react-rotating-text";
+
+import "./App.scss";
+import Particles from "./components/Particles.jsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Particles />
+      <nav className="App-nav">
+        <h1>OB</h1>
+        <button>
+          Say Hello{" "}
+          <span role="img" aria-label="wave">
+            👋
+          </span>
+        </button>
+      </nav>
+      <header className="App-header">
+        <Title></Title>
+        <h1 id="typewriter">
+          <RotatingText
+            items={[
+              "Web Design",
+              "Back-end Development",
+              "React Native App Development"
+            ]}
+          />
+        </h1>
+      </header>
+      <div className="content" id="skills">
+        <div>
+          <h1>Web Development</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+            obcaecati eos inventore, similique nostrum modi neque provident aut
+            itaque? Incidunt nemo fugit quasi temporibus debitis numquam
+            laboriosam, quae ipsam aut.
+          </p>
+        </div>
+        <div>
+          <h1>Server</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet ad
+            accusamus praesentium aliquam cupiditate eos saepe suscipit error
+            accusantium commodi enim ipsa, quae qui incidunt consectetur
+            debitis? Rem, autem neque?
+          </p>
+        </div>
+        <div>
+          <h1>App Development</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero
+            cupiditate tenetur eligendi numquam nemo voluptatibus, iure nobis
+            voluptatem provident, excepturi eveniet dicta aspernatur ad! Quos
+            aperiam voluptatem excepturi modi quibusdam.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
