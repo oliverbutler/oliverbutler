b8aa2c75b8bdf2426f20c7c07ee0b5a9dba68b6d
Oliver Butler <dev@oliverbutler.uk>
2021-04-02 12:55:49 +0100

    ✨ Add Strapi + BlurHash(!)

---
+++ strapi/api/post/models/post.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const slugify = require("slugify");
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title) {
+        data.slug = slugify(data.title);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.title) {
+        data.slug = slugify(data.title);
+      }
+    },
+  },
+};
--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.2jbbw.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'dbUser'),
-        password: env('DATABASE_PASSWORD', 'Wake-Placard-Greeter-Boring-Screen-Skillful7-Visitor'),
+        host: env("DATABASE_HOST", "cluster0.2jbbw.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "dbUser"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
