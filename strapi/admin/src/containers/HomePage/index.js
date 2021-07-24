7e59fc8882e6f11a349f875d924ef43d8ba3faa6
Oliver Butler <dev@oliverbutler.uk>
2021-07-24 21:16:24 +0100

    Updated admin home page

---
+++ strapi/admin/src/containers/HomePage/index.js
@@ -0,0 +1,102 @@
+/*
+ *
+ * HomePage
+ *
+ */
+/* eslint-disable */
+import React, { memo, useMemo } from "react";
+import { FormattedMessage } from "react-intl";
+import { get, upperFirst } from "lodash";
+import { auth, LoadingIndicatorPage } from "strapi-helper-plugin";
+import PageTitle from "../../components/PageTitle";
+import { useModels } from "../../hooks";
+
+import { Block, Container, P, Wave, Separator } from "./components";
+import SocialLink from "./SocialLink";
+
+const SOCIAL_LINKS = [
+  {
+    name: "GitHub Repo",
+    link: "https://github.com/oliverbutler/oliverbutler/",
+  },
+];
+
+const HomePage = ({ history: { push } }) => {
+  const {
+    collectionTypes,
+    singleTypes,
+    isLoading: isLoadingForModels,
+  } = useModels();
+
+  const hasAlreadyCreatedContentTypes = useMemo(() => {
+    const filterContentTypes = (contentTypes) =>
+      contentTypes.filter((c) => c.isDisplayed);
+
+    return (
+      filterContentTypes(collectionTypes).length > 1 ||
+      filterContentTypes(singleTypes).length > 0
+    );
+  }, [collectionTypes, singleTypes]);
+
+  if (isLoadingForModels) {
+    return <LoadingIndicatorPage />;
+  }
+
+  const headerId = hasAlreadyCreatedContentTypes
+    ? "HomePage.greetings"
+    : "app.components.HomePage.welcome";
+  const username = get(auth.getUserInfo(), "firstname", "");
+
+  return (
+    <>
+      <FormattedMessage id="HomePage.helmet.title">
+        {(title) => <PageTitle title={title} />}
+      </FormattedMessage>
+      <Container className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8 col-md-12">
+            <Block>
+              <Wave />
+              <FormattedMessage
+                id={headerId}
+                values={{
+                  name: upperFirst(username),
+                }}
+              >
+                {(msg) => <h2 id="mainHeader">{msg}</h2>}
+              </FormattedMessage>
+              <P>Welcome to Oliver Butlers blog, Strapi admin panel.</P>
+            </Block>
+          </div>
+
+          <div className="col-md-12 col-lg-4">
+            <Block style={{ paddingRight: 30, paddingBottom: 0 }}>
+              <h2>Handy Links</h2>
+
+              <P style={{ marginTop: 7, marginBottom: 0 }}>
+                Here are some handy links 😊
+              </P>
+
+              <Separator style={{ marginTop: 18 }} />
+              <div
+                className="row social-wrapper"
+                style={{
+                  display: "flex",
+                  margin: 0,
+                  marginTop: 36,
+                  marginLeft: -15,
+                }}
+              >
+                {SOCIAL_LINKS.map((value, key) => (
+                  <SocialLink key={key} {...value} />
+                ))}
+              </div>
+            </Block>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default memo(HomePage);
