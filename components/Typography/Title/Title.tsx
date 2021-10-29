1b24debf23aef384869d3c35dbfb55ff5d2cdce8
Oliver Butler <dev@oliverbutler.uk>
2021-10-29 11:38:27 +0100

    💄 remove coloured title

--- components/Typography/Title/Title.tsx
+++ components/Typography/Title/Title.tsx
@@ -16,12 +16,7 @@ export const Title: React.FunctionComponent<Props> = ({
     <div className=" w-full flex pt-5 items-center justify-center flex-col ">
       {image}
       <div className="text-center lg:w-2/3 w-full">
-        <Typography
-          variant="h1"
-          className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
-        >
-          {title}
-        </Typography>
+        <Typography variant="h1">{title}</Typography>
         <Typography variant="p" className="leading-relaxed mb-8">
           {subtitle}
         </Typography>
