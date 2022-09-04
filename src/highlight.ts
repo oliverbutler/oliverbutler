42d2bd876f116384f0dd5ebf1df7b343f1d7516d
Oliver Butler <dev@oliverbutler.uk>
2022-09-04 13:21:46 +0100

    feat: Fix images and add Gif back to strapi

--- src/highlight.ts
+++ src/highlight.ts
@@ -3,10 +3,12 @@ import type { Post } from "./posts";
 export type Highlight =
   | {
       type: "post";
+      class?: string;
       post: Post;
     }
   | {
       type: "custom";
+      class?: string;
       title: string;
       content: string;
       img: string;
