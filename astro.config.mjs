9fc7cea84ca7426ed3cb2f0cd33647e5cbf829d7
Oliver Butler <dev@oliverbutler.uk>
2023-09-08 01:16:21 +0100

    feat: remove base css

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,9 @@ export default defineConfig({
     }),
     sitemap(),
     tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
     }),
   ],
 });
